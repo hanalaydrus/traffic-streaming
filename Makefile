40caf1c6f131de8415902d215bc23e807131fef1
hanalaydrus <hanalaydrus@gmail.com>
2018-02-26 22:49:23 +0700

    delete wsgi file

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 run:
-	gunicorn --worker-class gthread --threads 5 -t 3600 -b 127.0.0.1:5000 wsgi:app
+	gunicorn --worker-class gthread --threads 5 -t 3600 -b 127.0.0.1:5000 main:app
--- main.py
+++ main.py
@@ -20,7 +20,6 @@ def index():
 def video_feed():
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-    
+                    
+if __name__ == "__main__":
+    app.run()
