8351614f1df204d92a64e2321172a2f64eda9da4
hanalaydrus <hanalaydrus@gmail.com>
2018-03-13 12:12:07 +0700

    all camera

--- camera.py
+++ camera.py
@@ -3,9 +3,10 @@ import threading
 import time
 
 class VideoCamera(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.video = cv2.VideoCapture('CarsDrivingUnderBridge.mp4')
+    def __init__(self, video_url):
+        threading.Thread.__init__(self)        
+        self.video = cv2.VideoCapture(video_url)
+        self.video_url = video_url
         success, image = self.video.read()
         ret, jpeg = cv2.imencode('.jpg',image)
         self.frame = jpeg.tobytes()
@@ -15,10 +16,9 @@ class VideoCamera(threading.Thread):
         while True:
             success, image = self.video.read()
             frame_count += 1
-            
             if frame_count == self.video.get(cv2.CAP_PROP_FRAME_COUNT):
                 frame_count = 0
-                self.video = cv2.VideoCapture('CarsDrivingUnderBridge.mp4')
+                self.video = cv2.VideoCapture(self.video_url)
                 success, image = self.video.read()
             ret, jpeg = cv2.imencode('.jpg',image)
             self.frame = jpeg.tobytes()
